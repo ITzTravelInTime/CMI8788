4a37c84e6ed83bd8092a8d2f8d0bc2ac136a255d
Gagan <Gagan@gagansmacpro.ed.shawcable.net>
2018-04-15 16:04:12 -0600

    uncomment gpio_changed fn pointer, which should work now. uart+gpio down. spdif/mpu/ac97+handlers left b4 volumectrls/streams etc.

--- CMI8788/XonarAudioEngine.cpp
+++ CMI8788/XonarAudioEngine.cpp
@@ -174,7 +174,7 @@ void XonarAudioEngine::xonar_init_ext_power(struct oxygen *chip)
     oxygen_set_bits8(chip, data->ext_power_int_reg,
                      data->ext_power_bit);
     chip->interrupt_mask |= OXYGEN_INT_GPIO;
-   // chip->model.gpio_changed = xonar_ext_power_gpio_changed;
+    chip->model.gpio_changed = xonar_ext_power_gpio_changed;
     data->has_power = !!(oxygen_read8(chip, data->ext_power_reg)
                          & data->ext_power_bit);
 }
